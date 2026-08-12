6138c225c43e83fbcd57c24adc63a69565e6b5c1
Navaneeth Rameshan <navaneeth.rameshan@gmail.com>
2026-08-12 07:35:29 +0200

    Revert "add AES-CBC encrypt and decrypt"
    This reverts commit 47bbab2d70f7146f895dff72d6fe16f774574dff.

--- internal/provider/software/symmetric_test.go
+++ internal/provider/software/symmetric_test.go
@@ -23,7 +23,6 @@ func aesCBCDetails(keySizeBits uint32) *types.AlgorithmDetails {
 		Algorithm: &types.AlgorithmDetails_AesCbc{
 			AesCbc: &types.AesCbcParams{
 				KeySizeBits: keySizeBits,
-				IvSizeBits:  128,
 				Padding:     types.PaddingScheme_PADDING_SCHEME_PKCS7,
 			},
 		},
